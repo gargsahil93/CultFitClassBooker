5454413690e7bf63d3c0b20396f0e79af4699231
Sahil Garg <s.sahgarg+github@gmail.com>
2022-03-25 08:08:48 +0530

    introduce book now

--- src/main/java/com/github/pnpninja/cultfitclassbooker/controller/KeepAliveController.java
+++ src/main/java/com/github/pnpninja/cultfitclassbooker/controller/KeepAliveController.java
@@ -1,17 +1,26 @@
 package com.github.pnpninja.cultfitclassbooker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KeepAliveController {
+
+	@Autowired
+	private ScheduledBookingTask task;
 	
 	@RequestMapping(value="/")
 	public int keepAlive() {
 		return 200;
 	}
 	
+	@RequestMapping(value="/trynow")
+	public void tryNow() {
+		// ScheduledBookingTask task = new ScheduledBookingTask();
+		task.bookClassesInAdvance();
+	}
 	
 
 }
